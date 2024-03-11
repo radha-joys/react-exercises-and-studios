a5ab82b2b412833582f316ece9a8e815997999a7
unknown <radhasrivepuri@gmail.com>
2024-03-11 14:50:41 -0500

    Class 19 Part2 exercise a,b completed

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,12 +1,22 @@
 import './styles.css';
 import Button from './Button';
-
+import oceans from './oceans.json';
 
 function Profile()
 {
+   const listItems = oceans.map((ocean) => {
+      return (
+         <div key={ocean.id}>
+            <img src={ocean.image} alt={ocean.name} className="img"/>
+         </div>
+      )
+   });
+
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+         <ul>
+            {listItems}
+         </ul>
          <Button />
       </>  
    );
