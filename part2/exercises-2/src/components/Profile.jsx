9158bcf88893551e8a24e53c84378fedf5b83ce4
unknown <radhasrivepuri@gmail.com>
2024-03-12 14:57:22 -0500

    Class 19 Exercises: React, Part 2

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -6,19 +6,24 @@ function Profile()
 {
    const listItems = oceans.map((ocean) => {
       return (
-         <div key={ocean.id}>
+         <div key={ocean.id} className={ocean.fishCheck === "true" ? "isAFish" : "profile"}>
             <img src={ocean.image} alt={ocean.name} className="img"/>
+            <h1>{ocean.name}</h1>
+            <h4>Fun Facts:</h4>
+            <ol>
+               <li>{ocean.fact1}</li>
+               <li>{ocean.fact2}</li>
+               <li>{ocean.fact3}</li>
+            </ol>
+            <Button />
          </div>
       )
    });
 
    return(
-      <>
-         <ul>
-            {listItems}
-         </ul>
-         <Button />
-      </>  
+      <ul>
+         {listItems}
+      </ul>
    );
 }
 
